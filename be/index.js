e622ed1bd9968d04c818f61a579d9e89135fd7a7
Aaron Echler <acechler@gmail.com>
2024-05-23 17:19:32 -0400

    update database

--- be/index.js
+++ be/index.js
@@ -1,20 +1,21 @@
+// index.js
 const express = require('express');
 const db = require('./db');
-require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.get('/', (req, res) => {
-  db.select('*').from('some_table') // Replace 'some_table' with your table name
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
-      res.status(500).json({ error: err.message });
-    });
+app.get('/users', async (req, res) => {
+  try {
+    const users = await db('users').select('*');
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
+  //console.table(db);
 });
+
