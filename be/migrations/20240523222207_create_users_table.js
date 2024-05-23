ed454e76df328c9faefae4cbd52b563cf19cc7ff
Aaron Echler <acechler@gmail.com>
2024-05-23 18:47:51 -0400

    Get database to connect

---
+++ be/migrations/20240523222207_create_users_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("states", function (table) {
+        table.increments("id").primary();
+        table.string("name").notNullable().unique();
+        table.string("capital").notNullable();
+        table.timestamps(true, true);
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable("states");
+};
