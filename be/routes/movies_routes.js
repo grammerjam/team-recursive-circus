9ef822f36c1fb19ed9b58d2aaab17b96c431e151
Cash Panico <rachelpanico@Cashs-Air.home>
2024-05-26 15:59:49 -0400

    uses routes

--- be/routes/movies_routes.js
+++ be/routes/movies_routes.js
@@ -2,14 +2,46 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/db.js");
 
-router.get("/movies", async (req, res) => {
-    try {
-      const movies = await db("movie_data");
-      res.json(movies);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Internal Server Error");
-    }
-  });
+router.get("/", async (req, res) => {
+  try {
+    const movies = await db("movie_data")
+      .select(
+        "movie_data.*",
+        db.raw(`
+          JSON_OBJECT(
+            'small', regular_images.small,
+            'medium', regular_images.medium,
+            'large', regular_images.large
+          ) as regular_images
+        `),
+        db.raw(`
+          JSON_OBJECT(
+            'small', trending_images.small,
+            'large', trending_images.large
+          ) as trending_images
+        `)
+      )
+      .leftJoin("regular_images", "movie_data.id", "regular_images.media_id")
+      .leftJoin("trending_images", "movie_data.id", "trending_images.media_id")
+      .groupBy(
+        "movie_data.id",
+        "regular_images.small",
+        "regular_images.medium",
+        "regular_images.large",
+        "trending_images.small",
+        "trending_images.large"
+      );
 
-  module.exports = router;
+    const formattedMovies = movies.map((movie) => ({
+      ...movie,
+      regular_images: JSON.parse(movie.regular_images || "{}"),
+      trending_images: JSON.parse(movie.trending_images || "{}"),
+    }));
+
+    res.json(formattedMovies);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
