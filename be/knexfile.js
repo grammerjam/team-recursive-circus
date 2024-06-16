bdcaf0222fdb78cb2b7961db802ba1d1d3529da7
Cash Panico <rachelpanico@hotmail.com>
2024-06-16 16:48:39 -0400

    added env vars for remote mysql

--- be/knexfile.js
+++ be/knexfile.js
@@ -5,10 +5,10 @@ module.exports = {
   development: {
     client: "mysql",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: process.env.MYSQL_ADDON_HOST,
+      user: process.env.MYSQL_ADDON_USER,
+      password: process.env.MYSQL_ADDON_PASSWORD,
+      database: process.env.MYSQL_ADDON_DB,
       debug: true
     },
     migrations: {
