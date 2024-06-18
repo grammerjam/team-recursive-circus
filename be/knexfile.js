5127e9b6ec3281b955f140af526bb2634ad3e74d
Cash Panico <rachelpanico@hotmail.com>
2024-06-18 12:15:39 -0400

    connection uri on knexfile

--- be/knexfile.js
+++ be/knexfile.js
@@ -4,13 +4,7 @@ console.log()
 module.exports = {
   development: {
     client: "mysql",
-    connection: {
-      host: process.env.MYSQL_ADDON_HOST,
-      user: process.env.MYSQL_ADDON_USER,
-      password: process.env.MYSQL_ADDON_PASSWORD,
-      database: process.env.MYSQL_ADDON_DB,
-      debug: true
-    },
+    connection: process.env.CONNECTION_URI,
     migrations: {
       directory: "./migrations",
     },
