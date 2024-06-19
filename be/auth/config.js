b70a099c59c5862ce04dad96a317270d87840ca5
Cash Panico <rachelpanico@hotmail.com>
2024-06-19 11:44:31 -0400

    auth

---
+++ be/auth/config.js
@@ -0,0 +1,3 @@
+const SECRET_KEY = process.env.SECRET_KEY || "shhhhhhhhhhhh";
+const DB_URI ="mysql://uhtrxxqnoj25otmw:kxg5CrVvBO8IrU94JhC6@b0jgdfu4xveyxsyw0bnn-mysql.services.clever-cloud.com:3306/b0jgdfu4xveyxsyw0bnn";
+const BCRYPT_WORK_FACTOR = 12;
