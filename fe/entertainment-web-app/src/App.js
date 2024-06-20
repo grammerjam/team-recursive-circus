e43efa5a2d7ddd045fd483ce047205cec6abec5c
Aaron Echler <acechler@gmail.com>
2024-06-20 12:23:12 -0400

    fix merge conflicts

--- fe/entertainment-web-app/src/App.js
+++ fe/entertainment-web-app/src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
-
+import SearchBar from "./components/Searchbar";
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="App flex flex-col lg:flex-row">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<div>Home Page</div>} />
-          <Route path="/movies" element={<div>Movies Page</div>} />
-          <Route path="/tv-series" element={<div>TV Series Page</div>} />
-          <Route path="/bookmark" element={<div>Bookmark Page</div>} />
-        </Routes>
-   
+        <div className="w-screen px-6 lg:mt-8 lg:px-0">
+          <SearchBar />
+          <Routes>
+            <Route path="/" element={<div>Home Page</div>} />
+            <Route path="/movies" element={<div>Movies Page</div>} />
+            <Route path="/tv-series" element={<div>TV Series Page</div>} />
+            <Route path="/bookmark" element={<div>Bookmark Page</div>} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
