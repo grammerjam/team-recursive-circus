b2742052a0422559d63f0b0b9bc4c77221be409a
Aaron Echler <acechler@gmail.com>
2024-06-20 11:51:40 -0400

    prepare to move signup page to its own route.

--- fe/entertainment-web-app/src/App.js
+++ fe/entertainment-web-app/src/App.js
@@ -3,21 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 
+
 function App() {
   return (
     <Router>
       <div className="App flex flex-col lg:flex-row">
       <div className="App flex flex-col lg:flex-row">
         <Navbar />
-        <div className="w-screen px-6 lg:mt-8 lg:px-0">
+        <Routes>
+          <Route path="/" element={<div>Home Page</div>} />
+          <Route path="/movies" element={<div>Movies Page</div>} />
+          <Route path="/tv-series" element={<div>TV Series Page</div>} />
+          <Route path="/bookmark" element={<div>Bookmark Page</div>} />
+        </Routes>
    
-          <Routes>
-            <Route path="/" element={<div>Home Page</div>} />
-            <Route path="/movies" element={<div>Movies Page</div>} />
-            <Route path="/tv-series" element={<div>TV Series Page</div>} />
-            <Route path="/bookmark" element={<div>Bookmark Page</div>} />
-          </Routes>
-        </div>
       </div>
     </Router>
   );
